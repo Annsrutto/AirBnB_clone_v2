56d488b1e5649e8248948d0e44a3dd217d11fc03
Annsrutto <annsrutto567@gmail.com>
2024-03-28 01:46:48 +0300

    Improve engines

--- models/state.py
+++ models/state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-import os
-
+from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
